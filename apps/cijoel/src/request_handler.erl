14e3669ca3c763128317224abd113ee91dbb55be
Justin Marney <gotascii@gmail.com>
2010-08-19 07:36:50 -0400

    organization tweaks, handle static files

--- apps/cijoel/src/request_handler.erl
+++ apps/cijoel/src/request_handler.erl
@@ -3,31 +3,51 @@
 	
 do(Info) ->
   Request = build_request(Info),
-	Data = route(Request:request_method(), Request:path()),
+	Data = generate_response_data(Request),
 	build_response(Data, Info).
 
 project_path(RequestPath) ->
-  lists:concat(["./site", RequestPath]).
+  lists:concat(["site", RequestPath]).
 
-route(RequestMethod, RequestPath) ->
-  ProjectPath = project_path(RequestPath),
-  case RequestMethod of
-    'GET' -> handle_get(ProjectPath);
-    'POST' -> handle_post(ProjectPath)
+static_path(RequestPath) ->
+  lists:concat(["site/public", RequestPath]).
+
+generate_response_data(Request) ->
+  RequestPath = Request:path(),
+  StaticPath = static_path(RequestPath),
+  StaticPathExists = filelib:is_regular(StaticPath),
+  if
+    StaticPathExists ->
+      handle_static(RequestPath);
+    true ->
+      RequestMethod = Request:request_method(),
+      ProjectPath = project_path(RequestPath),
+      case RequestMethod of
+        'GET' -> handle_get(ProjectPath);
+        'POST' -> handle_post(ProjectPath)
+      end
   end.
 
+handle_static(RequestPath) ->
+  {static, RequestPath}.
+
 handle_get(ProjectPath) ->
-  lists:concat(["GET ", ProjectPath]).
+  {dynamic, lists:concat(["GET ", ProjectPath])}.
 
 handle_post(ProjectPath) ->
-  lists:concat(["POST ", ProjectPath]).
+  {dynamic, lists:concat(["POST ", ProjectPath])}.
 
 build_request(Info) ->
   simple_bridge:make_request(inets_request_bridge, Info).
 
-build_response(Data, Info) ->
-  ResponseBridge = simple_bridge:make_response(inets_response_bridge, Info),
-  Response1 = ResponseBridge:status_code(200),
-  Response2 = Response1:header("Content-Type", "text/html"),
-  Response3 = Response2:data(Data),
-  Response3:build_response().
+build_response({ResponseType, Data}, Info) ->
+  Bridge = simple_bridge:make_response(inets_response_bridge, Info),
+  Response = case ResponseType of
+    static ->
+      Bridge:file(Data);
+    dynamic ->
+      B1 = Bridge:status_code(200),
+      B2 = B1:header("Content-Type", "text/html"),
+      B2:data(Data)
+  end,
+  Response:build_response().
